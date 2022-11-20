f452335b0ee42ca1bdfb43b02cafe1b1f7e51c63
YuCao16 <yu.carl@outlook.com>
2022-11-20 00:02:55 +0000

    init

---
+++ test/test_unet.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+
+
+class ResidualConvBlock(nn.Module):
+    def __init__(
+        self, in_channels: int, out_channels: int, is_res: bool = False
+    ) -> None:
+        super().__init__()
+        """
+        standard ResNet style convolutional block
+        """
+        self.same_channels = in_channels == out_channels
+        self.is_res = is_res
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.is_res:
+            x1 = self.conv1(x)
+            x2 = self.conv2(x1)
+            # this adds on correct residual in case channels have increased
+            out = x + x2 if self.same_channels else x1 + x2
+            return out / 1.414
+        else:
+            x1 = self.conv1(x)
+            return self.conv2(x1)
+
+
+class UnetDown(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(UnetDown, self).__init__()
+        """
+        process and downscale the image feature maps
+        """
+        layers = [ResidualConvBlock(in_channels, out_channels), nn.MaxPool2d(2)]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+
+class UnetUp(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(UnetUp, self).__init__()
+        """
+        process and upscale the image feature maps
+        """
+        layers = [
+            nn.ConvTranspose2d(in_channels, out_channels, 2, 2),
+            ResidualConvBlock(out_channels, out_channels),
+            ResidualConvBlock(out_channels, out_channels),
+        ]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor, skip: torch.Tensor) -> torch.Tensor:
+        x = torch.cat((x, skip), 1)
+        x = self.model(x)
+        return x
+
+
+class EmbedFC(nn.Module):
+    def __init__(self, input_dim: int, emb_dim: int) -> None:
+        super(EmbedFC, self).__init__()
+        """
+        generic one layer FC NN for embedding things  
+        """
+        self.input_dim = input_dim
+        layers = [
+            nn.Linear(input_dim, emb_dim),
+            nn.GELU(),
+            nn.Linear(emb_dim, emb_dim),
+        ]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.view(-1, self.input_dim)
+        return self.model(x)
+
+
+class ContextUnet(nn.Module):
+    def __init__(
+        self, in_channels: int, n_feat: int = 256, n_classes: int = 10
+    ) -> None:
+        super(ContextUnet, self).__init__()
+
+        self.in_channels = in_channels
+        self.n_feat = n_feat
+        self.n_classes = n_classes
+
+        self.init_conv = ResidualConvBlock(in_channels, n_feat, is_res=True)
+
+        self.down1 = UnetDown(n_feat, n_feat)
+        self.down2 = UnetDown(n_feat, 2 * n_feat)
+
+        self.to_vec = nn.Sequential(nn.AvgPool2d(7), nn.GELU())
+
+        self.timeembed1 = EmbedFC(1, 2 * n_feat)
+        self.timeembed2 = EmbedFC(1, 1 * n_feat)
+        self.contextembed1 = EmbedFC(n_classes, 2 * n_feat)
+        self.contextembed2 = EmbedFC(n_classes, 1 * n_feat)
+
+        self.up0 = nn.Sequential(
+            # nn.ConvTranspose2d(6 * n_feat, 2 * n_feat, 7, 7), # when concat temb and cemb end up w 6*n_feat
+            nn.ConvTranspose2d(
+                2 * n_feat, 2 * n_feat, 7, 7
+            ),  # otherwise just have 2*n_feat
+            nn.GroupNorm(8, 2 * n_feat),
+            nn.ReLU(),
+        )
+
+        self.up1 = UnetUp(4 * n_feat, n_feat)
+        self.up2 = UnetUp(2 * n_feat, n_feat)
+        self.out = nn.Sequential(
+            nn.Conv2d(2 * n_feat, n_feat, 3, 1, 1),
+            nn.GroupNorm(8, n_feat),
+            nn.ReLU(),
+            nn.Conv2d(n_feat, self.in_channels, 3, 1, 1),
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        c: torch.Tensor,
+        t: torch.Tensor,
+        context_mask: torch.Tensor,
+    ) -> torch.Tensor:
+        # x is (noisy) image, c is context label, t is timestep,
+        # context_mask says which samples to block the context on
+
+        x = self.init_conv(x)
+        down1 = self.down1(x)
+        down2 = self.down2(down1)
+        hiddenvec = self.to_vec(down2)
+
+        # convert context to one hot embedding
+        c = nn.functional.one_hot(c, num_classes=self.n_classes).type(
+            torch.float
+        )
+
+        # mask out context if context_mask == 1
+        context_mask = context_mask[:, None]
+        context_mask = context_mask.repeat(1, self.n_classes)
+        context_mask = -1 * (1 - context_mask)  # need to flip 0 <-> 1
+        c = c * context_mask
+
+        # embed context, time step
+        cemb1 = self.contextembed1(c).view(-1, self.n_feat * 2, 1, 1)
+        temb1 = self.timeembed1(t).view(-1, self.n_feat * 2, 1, 1)
+        cemb2 = self.contextembed2(c).view(-1, self.n_feat, 1, 1)
+        temb2 = self.timeembed2(t).view(-1, self.n_feat, 1, 1)
+
+        # could concatenate the context embedding here instead of adaGN
+        # hiddenvec = torch.cat((hiddenvec, temb1, cemb1), 1)
+
+        up1 = self.up0(hiddenvec)
+        # up2 = self.up1(up1, down2) # if want to avoid add and multiply embeddings
+        up2 = self.up1(
+            cemb1 * up1 + temb1, down2
+        )  # add and multiply embeddings
+        up3 = self.up2(cemb2 * up2 + temb2, down1)
+        out = self.out(torch.cat((up3, x), 1))
+        return out
+
+
+model = ContextUnet(1, 256, 10)
+x: torch.Tensor = torch.ones((256, 1, 28, 28))
+t: torch.Tensor = torch.ones((256))
+c: torch.Tensor = torch.randint_like(t, high=10, dtype=torch.int64)
+mask: torch.Tensor = torch.bernoulli(torch.zeros_like(c) + 0.1)
+
+# y = model(x, c, t, mask)  # output size (256, 1, 28, 28)
+
+# ----------------------------------
+# Model Visualization using Netron
+# ---------------------------------
+# input_names = [ "input image", "labels", "times", "context mask" ]
+# output_names = [ "output image" ]
+# torch.onnx.export(
+#     model,
+#     (x, c, t, mask),
+#     "unet.onnx",
+#     input_names = input_names,
+#     output_names=output_names,
+# )
+
+# -----------------------------------
+# Model Visualization using torchviz
+# -----------------------------------
+
+# from torchviz import make_dot
+# dot_easy = make_dot(y.mean(), params=dict(model.named_parameters()))
+# dot_complex = make_dot(y.mean(), params=dict(model.named_parameters()), show_attrs=True, show_saved=True)
+
+# dot_easy.format = format
+# dot_complex.format = format
+# dot_easy.render("test_easy.pdf")
+# dot_complex.render("test_complex.pdf")
